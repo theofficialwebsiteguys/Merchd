201e17bf43b4b4d808f16cf9f0fd22f9d82fa484
Full Name <theofficialwebsiteguys@gmail.com>
2024-11-28 14:21:44 -0500

    bugs

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Renderer2, OnInit } from '@angular/core';
-import { NavigationEnd, Router, ActivatedRoute } from '@angular/router';
+import { NavigationEnd, Router, ActivatedRoute, RouterModule } from '@angular/router';
 import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'] // Ensure this is "styleUrls" (plural)
 })
