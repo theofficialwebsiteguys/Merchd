6820f31bd439c4cab53de500dca9b8eb4d73a78b
Full Name <theofficialwebsiteguys@gmail.com>
2024-12-15 15:23:13 -0500

    hollis hobbys final review

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    api_url: 'https://ecommerce-api-db-a62e801c0f22.herokuapp.com',
-    stripePublicKey: ''
+    api_url: 'https://ecommerce-api-db-a62e801c0f22.herokuapp.com/api',
+    stripePublicKey: 'pk_test_51QUtysIokBL8AG53jGjmeRtOdC3fQzboBQf8YMC4IdvoAxld2Ey9uVqgjce3cRvlzZG7ZxJzkKQbTJiFOfbmAHlw00fJEI31b1'
   };
   
